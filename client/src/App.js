ecda62155928cf8301cf7b704235a3e7a9f58af5
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-06-25 15:06:20 +0200

    add style landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+// import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () =>
   <Router>
     <Fragment>
-      <Navbar />
+      {/* <Navbar /> */ }
       <Route exact path='/' component={ Landing } />
     </Fragment>
   </Router>;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,11 +5,33 @@ const Landing = () => {
   return (
     <section className="landing">
       <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Client Programming</h1>
-          <div className="buttons">
-            <Link to="/register" className="btn btn-primary">Sign Up</Link>
-            <Link to="/login" className="btn btn-light">Login</Link>
+        <div className="container">
+          <div className="card">
+            <div className="card-header">
+              <h3>Sign In</h3>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="input-group form-group">
+                  <span className="input-group-text"><i className="fas fa-user"></i>
+                    <input type="text" className="form-control" placeholder="username" />
+                  </span>
+                  <br />
+                  <span className="input-group-text"><i className="fas fa-key"></i>
+                    <input type="password" className="form-control" placeholder="password" />
+                  </span>
+
+                </div>
+                <div className="button">
+                  <Link to="/login" className="btn btn-primary">Login</Link>
+                </div>
+              </form>
+            </div>
+            <div className="card-footer">
+              <div className="d-flex justify-content-center links">
+                Don't have an account?<Link to="/register">Sign Up</Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>

2ff056b32d889dc722b37642157b8b73e46fe57c
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-06-28 16:51:53 +0200

    fix reset input form after submission

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -25,6 +25,13 @@ const Register = ({ setAlert, register }) => {
       setAlert('Passwords do not much', 'danger');
     } else {
       register({ firstName, lastName, email, password });
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        passwordRepeat: '',
+      });
     }
   };
 
@@ -39,33 +46,58 @@ const Register = ({ setAlert, register }) => {
           <div className="card-body">
             <form className="register-form" onSubmit={ (e) => onSubmit(e) }>
               <div className="register-input-group">
-                <span className="register-input-group-text">
-                  <input type="text" required className="form-control" name="firstName" value={ firstName } onChange={ (e) => onChange(e) } placeholder=" First name" />
-                </span>
-                <span className="register-input-group-text">
-                  <input type="text" className="form-control" name="lastName" value={ lastName } onChange={ (e) => onChange(e) } placeholder=" Last name (optional)" />
-                </span>
-                <span className="register-input-group-text">
-                  <input type="text" required className="form-control" name="email" value={ email } onChange={ (e) => onChange(e) } placeholder=" Email" />
-                </span>
-
-                <span className="register-input-group-text">
-                  <input type="password" required className="form-control" name="password" minLength="6" value={ password } onChange={ (e) => onChange(e) } placeholder=" Password" />
-                </span>
-                <span className="register-input-group-text">
-
-                  <input type="password" required className="form-control" name="passwordRepeat" minLength="6" value={ passwordRepeat } onChange={ (e) => onChange(e) } placeholder=" Confirm password" />
-                </span>
-
+                <input
+                  type="text"
+                  className="form-control"
+                  name="firstName"
+                  value={ firstName }
+                  onChange={ (e) => onChange(e) }
+                  placeholder=" First name"
+                  required />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="lastName"
+                  value={ lastName }
+                  onChange={ (e) => onChange(e) }
+                  placeholder=" Last name (optional)" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  value={ email }
+                  onChange={ (e) => onChange(e) }
+                  placeholder=" Email"
+                  required />
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  minLength="6"
+                  value={ password }
+                  onChange={ (e) => onChange(e) }
+                  placeholder=" Password"
+                  required />
+                <input
+                  type="password"
+                  className="form-control"
+                  name="passwordRepeat"
+                  minLength="6"
+                  value={ passwordRepeat }
+                  onChange={ (e) => onChange(e) }
+                  placeholder=" Confirm password"
+                  required />
               </div>
-              <input type="submit" className="input-register" value="Register" />
+              <input
+                type="submit"
+                className="input-register"
+                value="Register" />
             </form>
             <div className="card-footer">
               <div className="d-flex justify-content-center links">
                 Have an account?<Link to="/">Login</Link>
               </div>
             </div>
-
           </div>
         </div>
       </div>

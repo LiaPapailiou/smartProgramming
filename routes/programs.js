6f0f8259748a0b46036f21ff62811d55c21a72e6
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-07-31 15:31:32 +0200

    fix insert into Programs db

--- routes/programs.js
+++ routes/programs.js
@@ -2,8 +2,6 @@ const express = require('express');
 const { check, validationResult } = require('express-validator');
 // const auth = require('../middleware/auth');
 const router = express.Router();
-// const Clients = require('../model/Clients');
-// const Exercises = require('../model/Exercises');
 const Programs = require('../model/Programs');
 
 module.exports = router;
@@ -51,79 +49,126 @@ router.post('/add/:program_id', async (req, res) => {
   }
 });
 // Insert
-router.put('/insert',
-  [
-    // check('numberOfWeek', 'Week number is required').not().isEmpty(),
-    // check('percentage', 'Percentage is required').not().isEmpty(),
-    // check('reps_min', 'Minimum amount of reps is required').not().isEmpty(),
-    // check('reps_max', 'Maximum amount of reps is required').not().isEmpty(),
-    // check('sets', 'Number of sets is required').not().isEmpty(),
-    // check('numberOfDay', 'Day number is required').not().isEmpty(),
-    // check('exerciseList', 'Exercise name is required').not().isEmpty(),
-  ], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-    const { client_id, month, year, percentageOne, percentageTwo, percentageThree, percentageFour, reps_minOne, reps_minTwo, reps_minThree, reps_minOFour, reps_maxOne, reps_maxTwo, reps_maxThree, reps_maxFour, setsOne, setsTwo, setsThree, setsFour, exerciseListOne, exerciseListTwo, exerciseListThree, exerciseListFour } = req.body;
+router.put('/insert', async (req, res) => {
 
-    try {
-      // let program = await Programs.find({ client: client_id, month: month, year: year });
-      // if (program) return res.status(409).json({ errors: [{ msg: 'Program for that client already exists' }] });
-      const weekOne = {
-        percentageOne,
-        reps_minOne,
-        reps_maxOne,
-        setsOne,
-      };
+  const { client_id, month, year, percentageOne, percentageTwo, percentageThree, percentageFour, reps_minOne, reps_minTwo, reps_minThree, reps_minFour, reps_maxOne, reps_maxTwo, reps_maxThree, reps_maxFour, setsOne, setsTwo, setsThree, setsFour, exerciseListOne, exerciseListTwo, exerciseListThree, exerciseListFour } = req.body;
 
-      const weekTwo = {
-        percentageTwo,
-        reps_minTwo,
-        reps_maxTwo,
-        setsTwo,
-      };
+  try {
+    let program = await Programs.findOne({
+      $and: [{ month: month },
+      { year: year },
+      { client: client_id }]
+    });
 
-      const weekThree = {
-        percentageThree,
-        reps_minThree,
-        reps_maxThree,
-        setsThree,
-      };
+    if (program) return res.status(409).json({ errors: [{ msg: 'Program for that client already exists' }] });
 
-      const weekFour = {
-        percentageFour,
-        reps_minOFour,
-        reps_maxFour,
-        setsFour,
-      };
+    const weekOne = {
+      percentageOne,
+      reps_minOne,
+      reps_maxOne,
+      setsOne,
+    };
+
+    const weekTwo = {
+      percentageTwo,
+      reps_minTwo,
+      reps_maxTwo,
+      setsTwo,
+    };
+
+    const weekThree = {
+      percentageThree,
+      reps_minThree,
+      reps_maxThree,
+      setsThree,
+    };
+
+    const weekFour = {
+      percentageFour,
+      reps_minFour,
+      reps_maxFour,
+      setsFour,
+    };
 
 
-      program = await Programs.create({
-        // user: req.user.id,
-        client: client_id,
-        month,
-        year,
-        weekOne: weekOne,
-        'weekOne.0.exercisesOne.0.exerciseListOne': exerciseListOne,
-        weekTwo: weekTwo,
-        'weekTwo.0.exercisesTwo.0.exerciseListTwo': exerciseListTwo,
-        weekThree: weekThree,
-        'weekThree.0.exercisesThree.0.exerciseListThree': exerciseListThree,
-        weekFour: weekFour,
-        'weekFour.0.exercisesFour.0.exerciseListFour': exerciseListFour,
-      });
+    program = await Programs.create({
+      // user: req.user.id,
+      client: client_id,
+      month,
+      year,
+      weekOne: weekOne,
+      'weekOne.0.exercisesOne.0.exerciseListOne': exerciseListOne,
+      weekTwo: weekTwo,
+      'weekTwo.0.exercisesTwo.0.exerciseListTwo': exerciseListTwo,
+      weekThree: weekThree,
+      'weekThree.0.exercisesThree.0.exerciseListThree': exerciseListThree,
+      weekFour: weekFour,
+      'weekFour.0.exercisesFour.0.exerciseListFour': exerciseListFour,
+    });
 
-      res.json(program);
-    } catch (err) {
-      console.log(err.message);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+    res.json(program);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 // Edit
 router.post('/edit/:program_id', async (req, res) => {
+  const { client_id, month, year, percentageOne, percentageTwo, percentageThree, percentageFour, reps_minOne, reps_minTwo, reps_minThree, reps_minFour, reps_maxOne, reps_maxTwo, reps_maxThree, reps_maxFour, setsOne, setsTwo, setsThree, setsFour, exerciseListOne, exerciseListTwo, exerciseListThree, exerciseListFour } = req.body;
   try {
+    const weekOne = {
+      percentageOne,
+      reps_minOne,
+      reps_maxOne,
+      setsOne,
+    };
 
+    const weekTwo = {
+      percentageTwo,
+      reps_minTwo,
+      reps_maxTwo,
+      setsTwo,
+    };
+
+    const weekThree = {
+      percentageThree,
+      reps_minThree,
+      reps_maxThree,
+      setsThree,
+    };
+
+    const weekFour = {
+      percentageFour,
+      reps_minFour,
+      reps_maxFour,
+      setsFour,
+    };
+    let program = await Programs.findById(req.params.program_id);
+    if (!program) return res.status(404).json({ msg: 'Program not found in the database' });
+    program = await Programs.findByIdAndUpdate(
+      { _id: req.params.program_id },
+      {
+        $setOnInsert: {
+          // user: req.user.id,
+          client: client_id,
+          month,
+          year,
+          weekOne: weekOne,
+          'weekOne.0.exercisesOne.0.exerciseListOne': exerciseListOne,
+          weekTwo: weekTwo,
+          'weekTwo.0.exercisesTwo.0.exerciseListTwo': exerciseListTwo,
+          weekThree: weekThree,
+          'weekThree.0.exercisesThree.0.exerciseListThree': exerciseListThree,
+          weekFour: weekFour,
+          'weekFour.0.exercisesFour.0.exerciseListFour': exerciseListFour,
+        }
+      },
+      { new: true, upsert: true }
+    );
+    res.json(program);
   } catch (err) {
+    console.log(err.message);
     res.status(500).send('Internal Server Error');
   }
 });
